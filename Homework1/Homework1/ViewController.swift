f865b94967c0570ecdbca941d7a3f5876c56e8df
Ruslan Khanov <hanov@technokratos.com>
2021-09-19 17:29:15 +0300

    Add greetings button

--- Homework1/Homework1/ViewController.swift
+++ Homework1/Homework1/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private lazy var button = UIButton()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        button.setTitle("Hi!", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        view.addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            button.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20),
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
     }
-
-
 }
 
