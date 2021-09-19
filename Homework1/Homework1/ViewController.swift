fbc16fd9cdad05e1774dbbe1e5f96356e73f4798
Ruslan Khanov <hanov@technokratos.com>
2021-09-19 17:11:55 +0300

    Add greetings label

--- Homework1/Homework1/ViewController.swift
+++ Homework1/Homework1/ViewController.swift
@@ -9,9 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private lazy var label = UILabel()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        label.text = "Hello, World!"
+
+        view.addSubview(label)
+
+        label.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
 
 
