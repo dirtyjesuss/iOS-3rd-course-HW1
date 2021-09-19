7cf640ea4acb3f3961dd75e6c738f34c6869a142
dirtyjesuss <63318592+dirtyjesuss@users.noreply.github.com>
2021-09-19 18:24:15 +0300

    Greetings label
    * Add greetings label
    
    * Changed color, layer of greetings label

--- Homework1/Homework1/ViewController.swift
+++ Homework1/Homework1/ViewController.swift
@@ -9,20 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    private lazy var label = UILabel()
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.backgroundColor = .black
+        label.layer.cornerRadius = 3;
+        label.layer.masksToBounds = true;
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
 
-        label.text = "Welcome"
+        label.text = "Hello, World!"
 
         view.addSubview(label)
 
         label.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            label.topAnchor.constraint(equalTo: view.topAnchor, constant: 40)
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
         ])
     }
 
