5b5b3e99b665a3727ae3964605081a5bceae5b68
Ruslan Khanov <hanov@technokratos.com>
2021-09-19 17:01:37 +0300

    created initial project

---
+++ Homework1/Homework1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework1
+//
+//  Created by Ruslan Khanov on 19.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
